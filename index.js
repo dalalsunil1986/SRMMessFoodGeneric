c671313b666e64b7814ed51fbcb4ed5a844b60fc
T.R.Rohith <trrohith1@gmail.com>
2018-07-19 22:21:25 +0530

    connect to database

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
+var mysql = require('mysql');
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "srmmess",
+	password: "srmmess",
+	database: "srmmess"
+  });
 
 app.set('port', (process.env.PORT || 2050))
 
@@ -22,6 +28,16 @@ app.use(bodyParser.json());
 app.get('/', function (req, res) {
 	res.send('Hello world, I am a generic API')
 });
+//send mess array
+app.get('/messNames', function(req, res){
+        con.query(`SELECT name FROM mess`, function (err, result, fields) {
+            if (err) throw err;
+            for (var key in result) {
+                messNamesArray.push(result[key].name);
+            }
+            res.send(messNamesArray);
+        });
+})
 // Spin up the server
 app.listen(app.get('port'), function () {
 	console.log('running on port', app.get('port'))
