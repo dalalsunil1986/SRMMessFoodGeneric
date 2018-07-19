d801682bc9f9d5f92bd06d39b1478e10b844f022
T.R.Rohith <trrohith1@gmail.com>
2018-07-19 22:03:31 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+app.set('port', (process.env.PORT || 1911))
+
+//serve static files in the public directory
+app.use(express.static('public'));
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+// Process application/json
+app.use(bodyParser.json());
+
+
+// Index route
+app.get('/', function (req, res) {
+	res.send('Hello world, I am a generic API')
+});
+// Spin up the server
+app.listen(app.get('port'), function () {
+	console.log('running on port', app.get('port'))
+});
